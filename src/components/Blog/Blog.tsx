79f48ee4a5d08c90848a3a0b5629ff35fd605846
Olga Runova <ockaaa@gmail.com>
2021-09-30 12:24:29 +0300

    added blog

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import { RouteComponentProps } from 'react-router';
+import './blog.scss';
+import { allBlog } from '../../api/blogs';
 
-function Blog(): JSX.Element {
+type TParams = {id: string}
+
+function Blog({ match }: RouteComponentProps<TParams>): JSX.Element {
+  const id = Number(match.params.id);
+  
+  const isBlog = allBlog.filter(blog => blog.id === id);
+
+  const blog = isBlog[0];
+
+  if (isBlog.length === 0) {
+    return <div>Нет такого блога</div>
+  }
+
   return (
-    <h1>Blog</h1>
+    <section className="blog">
+      <h2 className="blog__title">{blog.title}</h2>
+    </section> 
   )
 }
 
